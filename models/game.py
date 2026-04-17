2d5520cd3cd84dbdc71ab29dbbec6089f4a73c57
NoVisDev <k25156719@kcl.ac.uk>
2026-04-17 21:59:22 +0100

    CREATE card states, still unused

--- models/game.py
+++ models/game.py
@@ -2,6 +2,98 @@ from config import FATE_LINE_LENGTH
 from utils.gm_utils import get_team
 from typing import List
 
+
+class NumberCardState:
+    # hold number card data
+
+    def __init__(self):
+        self._type = "number"  # immutable
+        self._number = None  # only between 5 and 0
+        self._label = None  # bound to string
+        self._choice = False  # default false
+
+    #region Multiple setters and getters via properties to trigger immutability or control type
+    @property
+    def type(self):
+        return self._type
+
+    @type.setter
+    def type(self, value):
+        # immutable, pass it
+        pass
+
+    @property
+    def number(self):
+        return self._number
+
+    @number.setter
+    def number(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Invalid type for number")
+        if value < 0 or value > 5:
+            raise ValueError("Invalid number for number")
+
+        self._number = value
+
+    @property
+    def label(self):
+        return self._label
+
+    @label.setter
+    def label(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Invalid type for label")
+        self._label = value
+
+    @property
+    def choice(self):
+        return self._choice
+
+    @choice.setter
+    def choice(self, value):
+        if not isinstance(value, bool):
+            raise ValueError("Invalid type for choice")
+        self._choice = value
+    #endregion
+
+
+class SpecialCardState:
+    def __init__(self):
+        self._type = "special"
+        self._action = None
+        self._label = None
+
+    #region Getters and setters for special card
+    @property
+    def type(self):
+        return self._type
+
+    @type.setter
+    def type(self, value):
+        # immutable, pass it
+        pass
+
+    @property
+    def action(self):
+        return self._action
+
+    @action.setter
+    def action(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Invalid type for action")
+        self._action = value
+
+    @property
+    def label(self):
+        return self._label
+
+    @label.setter
+    def label(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Invalid type for label")
+        self._label = value
+    #endregion
+
 class PlayerState:
     def __init__(self):
         self.name = None
@@ -47,13 +139,14 @@ class PlayerState:
         pass  # card count will never be mutated
     #endregion
 
+
 # TODO: Complete Game State and implement
 class GameState:
     def __init__(self):
         self.running_total = None
         self.current_player_idx = 0
         self.direction = 1
-        self.players : List[PlayerState] = []
+        self.players: List[PlayerState] = []
         self.fate_points = 0
         self.fate_line = FATE_LINE_LENGTH
         self.round = None
